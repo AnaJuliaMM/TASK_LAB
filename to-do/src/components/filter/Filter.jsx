b08a98c8819a1e9d471deaea8ee0c3015f3397c1
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-11-03 19:37:04 -0300

    style(sort): filter sort styled

--- to-do/src/components/filter/Filter.jsx
+++ to-do/src/components/filter/Filter.jsx
@@ -5,6 +5,10 @@ const Filter = ({filter, setFilter, sort, setSort}) => {
   return (
     <div className="filter">
         <div className="filter-options">
+            <div className="filter-sort">
+              <Button onClick={(e) => setSort('A-Z')} value='A-Z' style={{color: "black", width: 45, height:30, backgroundColor: '#e8e8e8', boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.15)' }}/>
+              <Button onClick={(e) => setSort('Z-A')} value='Z-A' style={{color: "black", width: 45, height:30, backgroundColor: '#e8e8e8',  boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.10)'}}/>
+            </div>
             <div className="filter-status">
                <p>Status</p> 
                <select value={filter} onChange={(e) => setFilter(e.target.value) }>
@@ -21,9 +25,3 @@ const Filter = ({filter, setFilter, sort, setSort}) => {
 
 export default Filter
 
-
-// {/* <div>
-//                <p>Ordem alfabética:</p> 
-//                 <Button onClick={(e) => setSort('A-Z')} value='A-Z'/>
-//                 <Button onClick={(e) => setSort('Z-A')} value='Z-A'/>
-//             </div> */}
